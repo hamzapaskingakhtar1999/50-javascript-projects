012600f0064fc4dd15d294651a2bdacfd2ed0950
Hamza Pasking Akhtar <hamzapaskingakhtar@gmail.com>
2024-12-28 01:03:41 +0000

    7. Split Landing Page CSS

--- 6. Scroll Animation/script.js
+++ 6. Scroll Animation/script.js
@@ -4,8 +4,10 @@ window.addEventListener("scroll", checkBoxes);
 
 function checkBoxes() {
   const triggerBottom = (window.innerHeight / 5) * 4;
+  console.log("Trigger Bottom: ", triggerBottom);
   boxes.forEach((box) => {
     const boxTop = box.getBoundingClientRect().top;
+    console.log("Box Top: ", boxTop);
 
     if (boxTop < triggerBottom) {
       box.classList.add("show");
@@ -14,3 +16,5 @@ function checkBoxes() {
     }
   });
 }
+
+checkBoxes();
